f75227cef7ff76f1cf74df00c17c9fe78ae43246
Valtkf <valentin.vgregorio@gmail.com>
2023-11-13 18:40:41 +0100

    version no scroll mobile

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ const overlay = document.querySelector(".overlay");
 menuHamburger.addEventListener("click", () => {
   navLinks.classList.toggle("mobile-menu");
   if (navLinks.classList.contains("mobile-menu")) {
-    body.style.overflow = "hidden";
+    body.style.overflow = "hidden"; // Empêche le défilement vertical
     overlay.style.display = "block";
   } else {
-    body.style.overflow = "auto";
+    body.style.overflow = "auto"; // Réactive le défilement vertical
     overlay.style.display = "none";
   }
 });
 
 overlay.addEventListener("click", () => {
   navLinks.classList.remove("mobile-menu");
-  body.style.overflow = "auto";
+  body.style.overflow = "auto"; // Réactive le défilement vertical
   overlay.style.display = "none";
 });
 
 navLinks.addEventListener("click", (event) => {
   if (event.target.tagName === "UL" || event.target.tagName === "LI") {
     navLinks.classList.remove("mobile-menu");
-    body.style.overflow = "auto";
+    body.style.overflow = "auto"; // Réactive le défilement vertical
     overlay.style.display = "none";
   }
 });
